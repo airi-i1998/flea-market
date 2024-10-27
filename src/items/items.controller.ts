8f1268f1d84fb2deee6bfeeeb2fd79792212b487
airi-i1998 <airi.inoue@generosity.co.jp>
2024-10-27 18:30:15 +0900

    add: find method

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { ItemsService } from './items.service';
 import { Item } from './items.model';
 
@@ -6,8 +6,13 @@ import { Item } from './items.model';
 export class ItemsController {
   constructor(private readonly itemsService: ItemsService) {}
   @Get()
-  findAll() {
+  findAll(): Item[] {
     return this.itemsService.findAll();
+  }
+
+  @Get(':id') // item/id
+  findById(@Param('id') id: string): Item {
+    return this.itemsService.findById(id)
   }
 
   @Post()
