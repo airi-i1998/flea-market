217dc637ac31a7cfd7303c6919af27c9d08b7630
airi-i1998 <airi.inoue@generosity.co.jp>
2024-10-27 19:55:08 +0900

    add: update status method

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
 import { ItemsService } from './items.service';
 import { Item } from './items.model';
 
@@ -12,7 +12,7 @@ export class ItemsController {
 
   @Get(':id') // item/id
   findById(@Param('id') id: string): Item {
-    return this.itemsService.findById(id)
+    return this.itemsService.findById(id);
   }
 
   @Post()
@@ -24,15 +24,20 @@ export class ItemsController {
     @Body('id') id: string,
     @Body('name') name: string,
     @Body('price') price: number,
-    @Body('description') description: string
+    @Body('description') description: string,
   ): Item {
     const item: Item = {
       id,
       name,
       price,
       description,
-      status: 'ON_SALE'
-    }
+      status: 'ON_SALE',
+    };
     return this.itemsService.create(item);
+  }
+
+  @Put(':id')
+  updateStatue(@Param('id') id: string) {
+    return this.itemsService.updateStatus(id);
   }
 }
